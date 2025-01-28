e2d44d0066aec6357cb954d92148955f82a5ad34
Chaichan <chaichanchaisak2549@gmail.com>
2025-01-28 16:12:51 +0700

    lab15_5.cpp completed

--- lab15_5.cpp
+++ lab15_5.cpp
@@ -20,3 +20,38 @@ int main(){
 
 //Do not modify the code above this line
 //Write definition of shuffle() using pointer after this line. 
+void shuffle(int * a ,int * b ,int * c ,int * d)
+{
+    int N = 4;
+    
+    int A[] = {*a , *b , *c , *d};
+    
+    int rand_[N];
+    rand_[0] = rand()%N;
+    
+    for (int i = 1 ; i < 4 ; i++)
+    {
+        while (true)
+        {
+            rand_[i] = rand()%4;
+            
+            bool pass = true;
+            
+            for (int j = 0 ; j < i ; j++)
+            {
+                if (rand_[i] == rand_[j])
+                {
+                    pass = false;
+                    break;
+                }
+            }
+            
+            if (pass == true) break;
+        }
+    }
+    
+    *a = A[rand_[0]];
+    *b = A[rand_[1]];
+    *c = A[rand_[2]];
+    *d = A[rand_[3]];
+}
